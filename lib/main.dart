c1581f3c830ac5b240845c986af110920a5cdca4
wonsun choi <wonsunny@kakao.com>
2023-04-03 18:08:52 +0900

    레이아웃 완성!

--- lib/main.dart
+++ lib/main.dart
@@ -11,21 +11,29 @@ class MyApp extends StatelessWidget {
 
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(),
+        appBar: AppBar(actions: [Icon(Icons.search),Icon(Icons.menu),Icon(Icons.notifications)], title: Text('아라일동'),
+          backgroundColor: Colors.orange,
+          elevation: 5,
+        ),
         body: Container(
-            height: 150, color: Colors.grey, padding: EdgeInsets.all(10),
+            height: 150, padding: EdgeInsets.all(10),
             child: Row (
               children: [
-                Image.asset("assets/rat.jpg", fit: BoxFit.cover,width: 150,height: 150,),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8.0),
+                    child: Image.asset(
+                      "assets/rat.jpg",
+                      fit: BoxFit.cover,width: 150,height: 150,)
+                ),
 
                 Flexible(
-                  child: Container(flex:,height: 150, color: Colors.cyan,
+                  child: Container(height: 150, margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text('캐논 DSLR 100D (단렌즈, 충전기 16기가 SD 포함)',style: TextStyle(fontSize: 20),),
-                        Text('성동구 행당동 . 끌올 10분 전'),
-                        Text('210,000원'),
+                        Text('졸귀탱 햄토리 (파는거아니고 자랑하려고올림)',style: TextStyle(fontSize: 20),),
+                        Text('제주시 아라일동 . 끌올 10분 전',style: TextStyle(fontSize: 10,color: Colors.grey),),
+                        Text('210,000원',style: TextStyle(fontSize: 15,fontWeight: FontWeight.w600),),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.end,
                           children: [
