861f3dcca4f4e3821703175ce263a4282b3ae5a6
wonsun choi <wonsunny@kakao.com>
2023-04-03 14:26:12 +0900

    당근따라하기!

--- lib/main.dart
+++ lib/main.dart
@@ -11,14 +11,33 @@ class MyApp extends StatelessWidget {
 
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('앱임')
-        ),
-        body: SizedBox(
-          child: ElevatedButton(
-            child: Text('글자'),
-            onPressed: (){},
-          )
+        appBar: AppBar(),
+        body: Container(
+            height: 150, color: Colors.grey, padding: EdgeInsets.all(10),
+            child: Row (
+              children: [
+                Image.asset("assets/rat.jpg", fit: BoxFit.cover,width: 150,height: 150,),
+
+                Flexible(
+                  child: Container(flex:,height: 150, color: Colors.cyan,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('캐논 DSLR 100D (단렌즈, 충전기 16기가 SD 포함)',style: TextStyle(fontSize: 20),),
+                        Text('성동구 행당동 . 끌올 10분 전'),
+                        Text('210,000원'),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Icon(Icons.favorite_border, color: Colors.grey,),
+                            Text('4')
+                          ],
+                        )
+                      ],
+                    ),),
+                )
+              ],
+            )
         ),
 
       )
